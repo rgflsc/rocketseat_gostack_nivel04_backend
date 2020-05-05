814d28e8131a4047157126b6531a07403820b091
Rodrigo Garcia <rgflsc@gmail.com>
2020-05-04 21:23:37 -0300

    Reescrevendo Repositórios
    Reescrevendo Repositórios

---
+++ src/modules/appointments/dtos/ICreateAppointmentDTO.ts
@@ -0,0 +1,4 @@
+export default interface ICreateApponimentDTO {
+  provider_id: string;
+  date: Date;
+}
--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,5 +1,8 @@
-import Appointment from '../infra/typeorm/entities/Appointment';
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+import ICreateApponimentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
 
 export default interface IAppointmentsRepository {
+  create(data: ICreateApponimentDTO): Promise<Appointment>;
+
   findByDate(date: Date): Promise<Appointment | undefined>;
 }
