ae7263bcda5461b6680ad855f2d60cab056c67c9
Rodrigo Garcia <rgflsc@gmail.com>
2020-05-07 21:05:09 -0300

    Testes de agendamento
    Testes de agendamento

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,10 +1,11 @@
 import FakeAppointmenstRepositry from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
+import AppError from '@shared/errors/AppError';
+
 describe('CreateAppointmen', () => {
   it('should be able to create a new appointment', async () => {
     const fakeAppointmenstRepositry = new FakeAppointmenstRepositry();
-
     const createAppointmentService = new CreateAppointmentService(fakeAppointmenstRepositry);
 
     const appointment = await createAppointmentService.execute({
@@ -16,7 +17,20 @@ describe('CreateAppointmen', () => {
     expect(appointment.provider_id).toBe('123456');
   });
 
-  it('should not be able to create two new appointment on the same time', () => {
-    expect(1 + 2).toBe(3);
+  it('should not be able to create two new appointment on the same time', async () => {
+    const fakeAppointmenstRepositry = new FakeAppointmenstRepositry();
+    const createAppointmentService = new CreateAppointmentService(fakeAppointmenstRepositry);
+
+    const appointmentDate = new Date(2020, 4, 10, 11);
+
+    await createAppointmentService.execute({
+      date: appointmentDate,
+      provider_id: '123456'
+    });
+
+    expect(createAppointmentService.execute({
+      date: appointmentDate,
+      provider_id: '123456'
+    })).rejects.toBeInstanceOf(AppError)
   });
 });
