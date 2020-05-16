828af403c1873d24fa24b4c0f96177f1ab5ecb74
Rodrigo Garcia <rgflsc@gmail.com>
2020-05-15 22:10:12 -0300

    Emails em desenvolvimento
    Emails em desenvolvimento

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -4,14 +4,14 @@ import IStorageProvider from '@shared/container/providers/StorageProviders/model
 import DiskStorageProvider from '@shared/container/providers/StorageProviders/implementatios/DiskStorageProvider';
 
 import IMailProvider from '@shared/container/providers/MailProviders/models/IMailProvider';
-//import DiskStorageProvider from '@shared/container/providers/StorageProviders/implementatios/DiskStorageProvider';
+import EtherealMailProvider from '@shared/container/providers/MailProviders/implementations/EtherealMailProvider';
 
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider,
 );
 
-container.registerSingleton<IMailProvider>(
+container.registerInstance<IMailProvider>(
   'MailProvider',
-  DiskStorageProvider,
+  new EtherealMailProvider(),
 );
