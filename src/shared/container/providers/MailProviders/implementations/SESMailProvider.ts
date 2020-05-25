b6e5a2387a49f559f87fadeb3a6cbc09b702d4aa
Rodrigo Garcia <rgflsc@gmail.com>
2020-05-25 20:02:25 -0300

    Emails pelo Amazon SES
    Emails pelo Amazon SES

---
+++ src/shared/container/providers/MailProviders/implementations/SESMailProvider.ts
@@ -0,0 +1,44 @@
+import nodemailer, { Transporter } from 'nodemailer';
+import { inject, injectable } from 'tsyringe';
+import aws from 'aws-sdk';
+import mailConfig from '@config/mail';
+
+import IMailProvider from '@shared/container/providers/MailProviders/models/IMailProvider';
+import IMailTemplateProvider from '@shared/container/providers/MailTemplateProvider/models/IMailTemplateProvider';
+import ISendMailDTO from '@shared/container/providers/MailProviders/dtos/ISendMailDTO';
+
+@injectable()
+class SESMailProvider implements IMailProvider {
+  private client: Transporter;
+
+  constructor(
+    @inject('MailTemplateProvider')
+    private mailTemplateProvider: IMailTemplateProvider,
+  ) {
+    this.client = nodemailer.createTransport({
+      SES: new aws.SES({
+        apiVersion: '2010-12-01',
+        region: 'us-east-1',
+      }),
+    })
+  };
+
+  public async sendMail({ to, from, subject, templateData }: ISendMailDTO): Promise<void> {
+    const { name, email } = mailConfig.defaults.from;
+
+    await this.client.sendMail({
+      to: {
+        name: to.name,
+        address: to.email,
+      },
+      from: {
+        name: from?.name || name,
+        address: from?.email || email,
+      },
+      subject,
+      html: await this.mailTemplateProvider.parse(templateData),
+    });
+  };
+}
+
+export default SESMailProvider;
--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,10 +1,12 @@
 import { container } from 'tsyringe';
+import mailConfig from '@config/mail';
 
 import IStorageProvider from '@shared/container/providers/StorageProviders/models/IStorageProvider';
 import DiskStorageProvider from '@shared/container/providers/StorageProviders/implementatios/DiskStorageProvider';
 
 import IMailProvider from '@shared/container/providers/MailProviders/models/IMailProvider';
 import EtherealMailProvider from '@shared/container/providers/MailProviders/implementations/EtherealMailProvider';
+import SESMailProvider from '@shared/container/providers/MailProviders/implementations/SESMailProvider';
 
 import IMailTemplateProvider from '@shared/container/providers/MailTemplateProvider/models/IMailTemplateProvider';
 import HandlebarsMailTemplateProvider from '@shared/container/providers/MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
@@ -21,5 +23,5 @@ container.registerSingleton<IMailTemplateProvider>(
 
 container.registerInstance<IMailProvider>(
   'MailProvider',
-  container.resolve(EtherealMailProvider),
+  mailConfig.driver === 'ethereal' ? container.resolve(EtherealMailProvider) : container.resolve(SESMailProvider),
 );
