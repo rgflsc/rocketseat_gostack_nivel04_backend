32d33a78734ecfd37389e0b17a17312b02a435c6
Rodrigo Garcia <rgflsc@gmail.com>
2020-05-26 19:23:41 -0300

    Upload de arquivos para o Amazon S3
    Upload de arquivos para o Amazon S3

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,20 +1,44 @@
 import path from 'path';
 import crypto from 'crypto';
-import multer from 'multer';
+import multer, { StorageEngine } from 'multer';
+import { string } from '@hapi/joi';
 
 const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 
+interface IUploadConfig {
+  driver: 'disk' | 's3';
+  tmpFolder: string;
+  uploadsFolder: string;
+  multer: {
+    storage: StorageEngine
+  };
+  config: {
+    disk: {},
+    aws: {
+      bucket: string;
+    },
+  }
+}
+
 export default {
+  driver: process.env.STORAGE_DRIVER || 'disk',
   tmpFolder: tmpFolder,
   uploadsFolder: path.resolve(tmpFolder, 'uploads'),
+  multer: {
+    storage: multer.diskStorage({
+      destination: tmpFolder,
+      filename(request, file, callback) {
+        const fileHash = crypto.randomBytes(10).toString('HEX');
+        const filename = `${fileHash}-${file.originalname}`;
 
-  storage: multer.diskStorage({
-    destination: tmpFolder,
-    filename(request, file, callback) {
-      const fileHash = crypto.randomBytes(10).toString('HEX');
-      const filename = `${fileHash}-${file.originalname}`;
-
-      return callback(null, filename);
+        return callback(null, filename);
+      },
+    }),
+  },
+  config: {
+    disk: {},
+    aws: {
+      bucket: '',
     },
-  }),
-};
+  }
+} as IUploadConfig;
--- src/shared/container/providers/StorageProviders/index.ts
+++ src/shared/container/providers/StorageProviders/index.ts
@@ -1,14 +1,17 @@
 import { container } from 'tsyringe';
+import uploadConfig from '@config/upload';
 
 import IStorageProvider from '@shared/container/providers/StorageProviders/models/IStorageProvider';
 
 import DiskStorageProvider from '@shared/container/providers/StorageProviders/implementatios/DiskStorageProvider';
+import S3StorageProvider from '@shared/container/providers/StorageProviders/implementatios/S3StorageProvider';
 
 const providers = {
   disk: DiskStorageProvider,
+  s3: S3StorageProvider,
 };
 
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
-  providers.disk,
+  providers[uploadConfig.driver],
 );
